8c96351dfe4f71772b82f620b80157b2ae444ea0
Joseph Francis <hookedupjoe@users.noreply.github.com>
2024-11-09 09:21:38 -0500

    log cleanup

--- appserver/actions/ws-main.js
+++ appserver/actions/ws-main.js
@@ -30,7 +30,6 @@ module.exports.setup = function setup(scope,options) {
                 tmpList[aID] = {name:tmpName, userid: tmpUserID, color: tmpColor, logo: tmpLogo};
             }
         }
-        console.log('people',tmpList)
         return tmpList;
     }    
 
@@ -69,12 +68,10 @@ module.exports.setup = function setup(scope,options) {
         if( users[theData.to] ){
             var tmpUser = users[theData.to];
             var tmpSocketID = tmpUser.socketid;
-            //console.log('req to tmpSocketID',tmpSocketID);
             wsRoom.sendDataToClient(tmpSocketID, {action:'meetingrequest', offer: theData.offer, fromid: theWS.userid, fromname: tmpName, message: 'Meeting request from ' + tmpName})
         } else {
             wsRoom.sendDataToClient(theWS.id, {action:'meetingreply', fromid: theWS.userid, status: false, message: 'No longer available'})  
         }
-        //wsRoom.sendDataToClient(theWS.id, {action:'chat', fromid: tmpUserID, fromname: tmpName, message: tmpMsg, toname: tmpNameTo})
 
     }
     
